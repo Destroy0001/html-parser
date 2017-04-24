8b8485b08ae3510062ef5082ea676744dcd473a0
Ashish Jhanwar <ashishjhanwar007@gmail.com>
2017-04-24 19:10:36 +0530

    Fixed bugs, added more documentation

--- app/controllers/concerns/parse_helper.rb
+++ app/controllers/concerns/parse_helper.rb
@@ -13,7 +13,7 @@ module ParseHelper
     end
 
     #parsing different elements from the html page
-    h1List = html.css('h1').map{ |h1| h1.inner_html.nil}
+    h1List = html.css('h1').map{ |h1| h1.inner_html }
     h2List = html.css('h2').map{ |h2| h2.inner_html }
     h3List = html.css('h3').map{ |h3| h3.inner_html }
     links = html.css('a').map{ |link| link['href'] }
@@ -22,19 +22,29 @@ module ParseHelper
 
     #if the url doesn't exist, create it, else update it. 
     if recordUrl.nil?
-      recordUrl = Url.new(url: url, h1: h1List, h2: h2List, h3: h3List, links: linkList)
+      recordUrl = Url.new(url: url, h1: h1List, h2: h2List, h3: h3List, links: links)
     else
       recordUrl.update(h1: h1List, h2: h2List, h3: h3List, links: links)
     end
     recordUrl.save
 
-    render :json => {:response => url + " was parsed sucessfully"}, :status => 200
+    #returning a success response
+    render :json => { :response => url + " was parsed sucessfully" }, :status => 200
   end
 
   #shows all stores urls 
   def showUrl()
-    #fetching all records,
-    recordUrls = Url.only(:url,:h1, :h2, :h3, :links)
+    #fetching all records, and then removing the _id key,
+    #inbuilt project methods don't filter the _id field
+    recordUrls = Url.all.map{
+      |url| { 
+        :url => url.url,
+        :h1 => url.h1,
+        :h2 => url.h2,
+        :h3=> url.h3,
+        :links => url.links 
+      }
+    }
     render :json => { :response => recordUrls }, :status =>200
   end
 
--- app/controllers/url_controller.rb
+++ app/controllers/url_controller.rb
@@ -8,12 +8,15 @@ class UrlController < ApplicationController
     if not url
       return badRequest()
     end
-
+    
+    #this is defined in ParseHelper concern
     parseUrl(url)
   end
 
   #shows all stored urls
   def show
+
+    #this is defined in ParseHelper concern
     showUrl()
   end
 
